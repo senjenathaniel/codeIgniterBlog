1ca5167fb4560ea286647f0032ab6d5caf649102
senjenathaniel <topsailcashew@icloud.com>
2022-06-23 09:52:07 +0300

    Passing data ID fails

--- app/Views/blog/blog_index.php
+++ app/Views/blog/blog_index.php
@@ -1,12 +1,13 @@
-<div style="padding-top: 4em;">
-    <div class="container">
+
+    <div class="container pt-5">
         <div class="row">
             <?php foreach ($blog as $post): ?>
             <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="<?=$post['blog_image_url'];?>" class="card-img-top" alt="...">
+                <div class="card mb-3" style="width: 18rem;">
+                    <img style="height: 17em;" src="<?=$post['blog_image_url'];?>" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4><?=substr($post['blog_title'], 0, 20);?></h4>
+                        <p><?=substr($post['blog_body'], 0, 60) . "...";?></p>
                         <p><a class="btn btn-primary" href="/blog/<?=$post['id'];?>">Read More</a></p>
                     </div>
                 </div>
@@ -14,4 +15,3 @@
             <?php endforeach;?>
         </div>
     </div>
-</div>
--- app/Views/blog/single_blog.php
+++ app/Views/blog/single_blog.php
@@ -1,11 +1,13 @@
-<div class="row">
-  <img src="<?=$img_url;?>" class="img-fluid" alt="...">
+<div class="mt-5 mb-5">
+ <img class="img-fluid rounded mx-auto d-block"  src="https://images.unsplash.com/photo-1462332420958-a05d1e002413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNwYWNlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60
+" alt="">
 </div>
+
 <div class="container">
   <div class="row">
     <h1 class="display-4"><?=$blog_title;?></h1>
     <b class="small"><?=$date_posted;?> | <?=$blog_author;?></b>
     <hr>
-    <?=$blog_body;?>
+    <div class="container"><?=$blog_body;?></div>
   </div>
 </div>
