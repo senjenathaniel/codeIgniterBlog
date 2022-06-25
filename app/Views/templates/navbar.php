2ac7f16ddd79c393ff936da219ab5a883cdc5ec3
senjenathaniel <topsailcashew@icloud.com>
2022-06-25 19:30:54 +0300

    Minimal changes to navigation

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -11,7 +11,7 @@
             <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
+            <a class="nav-link" href="/admin">Login</a>
           </li>
         </ul>
         <form class="d-flex">
